b2155c684afb58e7b224afda2c3e5642a1ac7d6e
ali rasouli <ali.r.programmer@gmail.com>
2024-04-20 10:03:36 +0330

    Update contactlist.js

--- wwwroot/netcharge/js/contactlist.js
+++ wwwroot/netcharge/js/contactlist.js
@@ -3,47 +3,57 @@ const props = ['name', 'tel'];
 const opts = { multiple: false };
 async function getContacts() {
 	try {
-		const contacts = await navigator.contacts.select(props, opts);
-		var strcontact = "";
-		for (var i = 0; i < contacts.length; i++) {
-
-			strcontact += `<li class="search_user_li" data-name="` + contacts[i].name +`" data-number="` + getNormalPhone(contacts[i].tel) +`">
-								<span class="search_user_name">`+ contacts[i].name +`</span>
-								<span class="search_user_icon"></span>
-							</li>`;
+		if (navigator.contacts) {
+			const contacts = await navigator.contacts.select(props, opts);
+			LoadContactSecondStep(contacts);
+			//}, 1000);
+			return false;
+			/*
+			
+			*/
 		}
-		$('.search_user_list_ul').html(strcontact);
-		//console.log('contacts:', contacts);
-		//$(".search_user_li").click((e) => {
-		//	$(".user_search_list_ok span").text($(e.currentTarget).data("number"));
-		//	let number = $(e.currentTarget).data("number");
-		//	number = number.substring(0, 4);
-		//	autoSelectOperatorType(number)
-		//	$(".user_search_list_ok").addClass("d-block");
-		//});
+		else {
+			JSBridge.LoadContact();
+		}
 
-		//setTimeout(function () {
-			var normalTel = getNormalPhone(contacts[0].tel[0]);
-			$(".form__input__mobile").val(normalTel);
-			//$(e.currentTarget).find("span").text("");
-			//$(e.currentTarget).removeClass("d-block");
-			$('.buy-credit-section .clear__input').addClass('d-block')
-			$(".buy-credit-section").removeClass("d-flex");
-			$(".buy-credit-step-1").addClass("d-flex");
-			$(".buy-credit-section").css('opacity', ".3")
-			$(".buy-credit-step-1").animate({ opacity: 1 }, 500);
-			$(".form__input__mobile").keyup()
-		//}, 1000);
-		return false;
-		/*
-		
-		*/
 	} catch (ex) {
 		// Handle any errors here.
-		alert(ex);
+		//alert(ex);
 		alert("لیست مخاطبین در این مرورگر یا در این سیستم عامل فعال نیست!");
 		return false;
 	}
+}
+function LoadContactSecondStep(contacts) {
+	var strcontact = "";
+	for (var i = 0; i < contacts.length; i++) {
+
+		strcontact += `<li class="search_user_li" data-name="` + contacts[i].name + `" data-number="` + getNormalPhone(contacts[i].tel) + `">
+								<span class="search_user_name">`+ contacts[i].name + `</span>
+								<span class="search_user_icon"></span>
+							</li>`;
+	}
+	$('.search_user_list_ul').html(strcontact);
+	//console.log('contacts:', contacts);
+	//$(".search_user_li").click((e) => {
+	//	$(".user_search_list_ok span").text($(e.currentTarget).data("number"));
+	//	let number = $(e.currentTarget).data("number");
+	//	number = number.substring(0, 4);
+	//	autoSelectOperatorType(number)
+	//	$(".user_search_list_ok").addClass("d-block");
+	//});
+
+	//setTimeout(function () {
+	var normalTel = getNormalPhone(contacts[0].tel[0]);
+	$(".form__input__mobile").val(normalTel);
+	//$(e.currentTarget).find("span").text("");
+	//$(e.currentTarget).removeClass("d-block");
+	$('.buy-credit-section .clear__input').addClass('d-block')
+	$(".buy-credit-section").removeClass("d-flex");
+	$(".buy-credit-step-1").addClass("d-flex");
+	$(".buy-credit-section").css('opacity', ".3")
+	$(".buy-credit-step-1").animate({ opacity: 1 }, 500);
+	$(".form__input__mobile").keyup();
+
 }
 function getNormalPhone(tel) {
     try {
